bbb9bcff8fa066b3ca80bfe6c3acbab699537da7
FeedSnow <mateovip22262@gmail.com>
2025-01-10 01:18:12 +0100

    book returning and reservation cancelling added

--- bookRent/routers/rental_router.py
+++ bookRent/routers/rental_router.py
@@ -1,16 +1,22 @@
-from fastapi import APIRouter
+from datetime import date
+
+from fastapi import APIRouter, Depends
+from sqlalchemy.orm import Session
 
 from bookRent.BooksCRUD.add.rental_add import create_rental
-from bookRent.BooksCRUD.get.rental_get import *
+import bookRent.BooksCRUD.get.rental_get as rg
 from bookRent.BooksCRUD.tools import try_perform
+from bookRent.BooksCRUD.update.rental_update import return_copy, return_my_copy
 from bookRent.db_config import get_db
 from bookRent.dependiencies import get_current_user, role_required
+from bookRent.models import rental_model
+from bookRent.models.models import User
 from bookRent.schematics.rental_schemas import RentalCreate, Rental
 
 router = APIRouter()
 
 # Worker
-@router.post("/add")
+@router.post("/add", response_model=Rental | None)
 def add(rental: RentalCreate, user: dict = Depends(get_current_user), role: str = Depends(role_required(['User', 'Worker', 'Admin'])), db: Session = Depends(get_db)):
     usr = db.query(User).filter_by(email=user["username"]).first()
     rental.user_id = usr.id
@@ -23,72 +29,88 @@ def add(rental: RentalCreate, user: dict = Depends(get_current_user), role: str 
 
 @router.get("/get-all", response_model=list[Rental] | None)
 def get_all(role: str = Depends(role_required(['Worker', 'Admin'])), db: Session = Depends(get_db)):
-    return try_perform(get_all_rentals, db=db)
+    return try_perform(rg.get_all_rentals, db=db)
 
 
 @router.get("/get-by-id", response_model=Rental | None)
 def get_by_id(id: int, role: str = Depends(role_required(['Worker', 'Admin'])), db: Session = Depends(get_db)):
-    return try_perform(get_rental_by_id, id, db=db)
+    return try_perform(rg.get_rental_by_id, id, db=db)
 
 
 @router.get("/get-by-user-id", response_model=Rental | list[Rental] | None)
 def get_by_user_id(id: int, role: str = Depends(role_required(['Worker', 'Admin'])), db: Session = Depends(get_db)):
-    return try_perform(get_rentals_by_user_id, id, db=db)
+    return try_perform(rg.get_rentals_by_user_id, id, db=db)
 
 
 @router.get("/get-rented-by-user-id", response_model=Rental | list[Rental] | None)
 def get_by_user_id(id: int, role: str = Depends(role_required(['Worker', 'Admin'])), db: Session = Depends(get_db)):
-    return try_perform(get_rentals_by_user_id_rented, id, db=db)
+    return try_perform(rg.get_rentals_by_user_id_rented, id, db=db)
 
 
 @router.get("/get-returned-by-user-id", response_model=Rental | list[Rental] | None)
 def get_by_user_id(id: int, role: str = Depends(role_required(['Worker', 'Admin'])), db: Session = Depends(get_db)):
-    return try_perform(get_rentals_by_user_id_returned, id, db=db)
+    return try_perform(rg.get_rentals_by_user_id_returned, id, db=db)
 
 
 @router.get("/get-by-copy-id", response_model=Rental | list[Rental] | None)
 def get_by_copy_id(id: int, role: str = Depends(role_required(['Worker', 'Admin'])), db: Session = Depends(get_db)):
-    return try_perform(get_rentals_by_copy_id, id, db=db)
+    return try_perform(rg.get_rentals_by_copy_id, id, db=db)
 
 
 @router.get("/get-by-rental-date", response_model=Rental | list[Rental] | None)
 def get_by_rental_date(rental_date: date, role: str = Depends(role_required(['Worker', 'Admin'])), db: Session = Depends(get_db)):
-    return try_perform(get_rentals_by_rental_date, rental_date, db=db)
+    return try_perform(rg.get_rentals_by_rental_date, rental_date, db=db)
 
 
 @router.get("/get-by-due-date", response_model=Rental | list[Rental] | None)
 def get_by_due_date(due_date: date, role: str = Depends(role_required(['Worker', 'Admin'])), db: Session = Depends(get_db)):
-    return try_perform(get_rentals_by_due_date, due_date, db=db)
+    return try_perform(rg.get_rentals_by_due_date, due_date, db=db)
 
 
 @router.get("/get-by-return-date", response_model=Rental | list[Rental] | None)
 def get_by_return_date(return_date: date, role: str = Depends(role_required(['Worker', 'Admin'])), db: Session = Depends(get_db)):
-    return try_perform(get_rentals_by_return_date, return_date, db=db)
+    return try_perform(rg.get_rentals_by_return_date, return_date, db=db)
 
 
 @router.get("/get_past_due", response_model=Rental | list[Rental] | None)
 def get_past_due(role: str = Depends(role_required(['Worker', 'Admin'])), db: Session = Depends(get_db)):
-    return try_perform(get_rentals_past_due, db=db)
+    return try_perform(rg.get_rentals_past_due, db=db)
 
 
 @router.get("/get_not_returned", response_model=Rental | list[Rental] | None)
 def get_not_returned(role: str = Depends(role_required(['Worker', 'Admin'])), db: Session = Depends(get_db)):
-    return try_perform(get_rentals_not_returned, db=db)
+    return try_perform(rg.get_rentals_not_returned, db=db)
 
 # User
 @router.get("/get-my", response_model=Rental | list[Rental] | None)
 def get_my(user: dict = Depends(get_current_user), role: str = Depends(role_required(['User', 'Worker', 'Admin'])), db: Session = Depends(get_db)):
     usr = db.query(User).filter_by(email=user["username"]).first()
-    return try_perform(get_rentals_by_user_id, usr.id, db=db)
+    return try_perform(rg.get_rentals_by_user_id, usr.id, db=db)
 
 # User
 @router.get("/get-my-rented", response_model=Rental | list[Rental] | None)
 def get_my(user: dict = Depends(get_current_user), role: str = Depends(role_required(['User', 'Worker', 'Admin'])), db: Session = Depends(get_db)):
     usr = db.query(User).filter_by(email=user["username"]).first()
-    return try_perform(get_rentals_by_user_id_rented, usr.id, db=db)
+    return try_perform(rg.get_rentals_by_user_id_rented, usr.id, db=db)
 
 # User
 @router.get("/get-my-returned", response_model=Rental | list[Rental] | None)
 def get_my(user: dict = Depends(get_current_user), role: str = Depends(role_required(['User', 'Worker', 'Admin'])), db: Session = Depends(get_db)):
     usr = db.query(User).filter_by(email=user["username"]).first()
-    return try_perform(get_rentals_by_user_id_returned, usr.id, db=db)
+    return try_perform(rg.get_rentals_by_user_id_returned, usr.id, db=db)
+
+
+# === UPDATE ===
+
+
+@router.put("/return-my", response_model=Rental | None)
+def return_my(id: int, user: dict = Depends(get_current_user), role: str = Depends(role_required(['User', 'Worker', 'Admin'])), db: Session = Depends(get_db)):
+    print(id)
+    usr = db.query(User).filter_by(email=user["username"]).first()
+    print(usr)
+    return try_perform(return_my_copy, id, usr.id, db=db)
+
+
+@router.put("/return", response_model=Rental | None)
+def return_rental(id: int, role: str = Depends(role_required(['Worker', 'Admin'])), db: Session = Depends(get_db)):
+    return try_perform(return_copy, id, db=db)
--- bookRent/routers/reservation_router.py
+++ bookRent/routers/reservation_router.py
@@ -1,19 +1,29 @@
-from fastapi import APIRouter
+from datetime import date
+
+from fastapi import APIRouter, Depends
+from sqlalchemy.orm import Session
 
 from bookRent.BooksCRUD.add.reservation_add import create_reservation
-from bookRent.BooksCRUD.get.reservation_get import *
+import bookRent.BooksCRUD.get.reservation_get as rg
 from bookRent.BooksCRUD.tools import try_perform
+from bookRent.BooksCRUD.update.reservation_update import cancel_reservation, cancel_reservations_by_copy_id, \
+    cancel_reservations_by_user_id, cancel_my_reservation
 from bookRent.db_config import get_db
 from bookRent.dependiencies import get_current_user, role_required
+from bookRent.models import reservation_model
+from bookRent.models.models import User
 from bookRent.schematics.reservation_schemas import ReservationCreate, Reservation
 
 router = APIRouter()
 
 # User
-@router.post("/add")
+@router.post("/add", response_model=Reservation | None)
 def make_reservation(reservation: ReservationCreate, user: dict = Depends(get_current_user), role: str = Depends(role_required(['User', 'Worker', 'Admin'])), db: Session = Depends(get_db)):
+    print(reservation)
     usr = db.query(User).filter_by(email=user["username"]).first()
+    print(usr)
     reservation.user_id = usr.id
+    print(reservation)
     return try_perform(create_reservation, reservation, db=db)
 
 
@@ -23,51 +33,75 @@ def make_reservation(reservation: ReservationCreate, user: dict = Depends(get_cu
 
 @router.get("/get-all", response_model=list[Reservation] | None)
 def get_all(role: str = Depends(role_required(['Worker', 'Admin'])), db: Session = Depends(get_db)):
-    return try_perform(get_all_reservations, db=db)
+    return try_perform(rg.get_all_reservations, db=db)
 
 
 @router.get("/get-by-id", response_model=Reservation | None)
 def get_by_id(id: int, role: str = Depends(role_required(['Worker', 'Admin'])), db: Session = Depends(get_db)):
-    return try_perform(get_reservation_by_id, id, db=db)
+    return try_perform(rg.get_reservation_by_id, id, db=db)
 
 
 @router.get("/get-by-user-id", response_model=Reservation | list[Reservation] | None)
 def get_by_user_id(id: int, role: str = Depends(role_required(['Worker', 'Admin'])), db: Session = Depends(get_db)):
-    return try_perform(get_reservations_by_user_id, id, db=db)
+    return try_perform(rg.get_reservations_by_user_id, id, db=db)
 
 
 @router.get("/get-reserved-by-user-id", response_model=Reservation | list[Reservation] | None)
 def get_by_user_id(id: int, role: str = Depends(role_required(['Worker', 'Admin'])), db: Session = Depends(get_db)):
-    return try_perform(get_reservations_by_user_id_reserved, id, db=db)
+    return try_perform(rg.get_reservations_by_user_id_reserved, id, db=db)
 
 
 @router.get("/get-by-copy-id", response_model=Reservation | list[Reservation] | None)
 def get_by_copy_id(id: int, role: str = Depends(role_required(['Worker', 'Admin'])), db: Session = Depends(get_db)):
-    return try_perform(get_reservations_by_copy_id, id, db=db)
+    return try_perform(rg.get_reservations_by_copy_id, id, db=db)
 
 
 @router.get("/get-by-reservation-date", response_model=Reservation | list[Reservation] | None)
 def get_by_reservation_date(reservation_date: date, role: str = Depends(role_required(['Worker', 'Admin'])), db: Session = Depends(get_db)):
-    return try_perform(get_reservations_by_date, reservation_date, db=db)
+    return try_perform(rg.get_reservations_by_date, reservation_date, db=db)
 
 
 @router.get("/get-by-due-date", response_model=Reservation | list[Reservation] | None)
 def get_by_due_date(due_date: date, role: str = Depends(role_required(['Worker', 'Admin'])), db: Session = Depends(get_db)):
-    return try_perform(get_reservations_by_due_date, due_date, db=db)
+    return try_perform(rg.get_reservations_by_due_date, due_date, db=db)
 
 
 @router.get("/get-by-status", response_model=Reservation | list[Reservation] | None)
 def get_by_status(status: str, role: str = Depends(role_required(['Worker', 'Admin'])), db: Session = Depends(get_db)):
-    return try_perform(get_reservations_by_status, status, db=db)
+    return try_perform(rg.get_reservations_by_status, status, db=db)
 
 # User
 @router.get("/get-my", response_model=Reservation | list[Reservation] | None)
 def get_my(user: dict = Depends(get_current_user), role: str = Depends(role_required(['User', 'Worker', 'Admin'])), db: Session = Depends(get_db)):
     usr = db.query(User).filter_by(email=user["username"]).first()
-    return try_perform(get_reservations_by_user_id, usr.id, db=db)
+    return try_perform(rg.get_reservations_by_user_id, usr.id, db=db)
 
 # User
 @router.get("/get-my-reserved", response_model=Reservation | list[Reservation] | None)
 def get_my(user: dict = Depends(get_current_user), role: str = Depends(role_required(['User', 'Worker', 'Admin'])), db: Session = Depends(get_db)):
     usr = db.query(User).filter_by(email=user["username"]).first()
-    return try_perform(get_reservations_by_user_id_reserved, usr.id, db=db)
+    return try_perform(rg.get_reservations_by_user_id_reserved, usr.id, db=db)
+
+
+# === UPDATE ===
+
+# User
+@router.put("/cancel-my", response_model=list[Reservation] | None)
+def cancel_my(id: int, user: dict = Depends(get_current_user), role: str = Depends(role_required(['User', 'Worker', 'Admin'])), db: Session = Depends(get_db)):
+    usr = db.query(User).filter_by(email=user["username"]).first()
+    return try_perform(cancel_my_reservation, id, usr.id, db=db)
+
+# Worker
+@router.put("/cancel", response_model=list[Reservation] | None)
+def cancel(id: int, role: str = Depends(role_required(['Worker', 'Admin'])), db: Session = Depends(get_db)):
+    return try_perform(cancel_reservation, id, db=db)
+
+# Admin
+@router.put("/cancel-by-copy-id", response_model=list[Reservation] | None)
+def cancel_by_copy_id(id: int, role: str = Depends(role_required(['Admin'])), db: Session = Depends(get_db)):
+    return try_perform(cancel_reservations_by_copy_id, id, db=db)
+
+# Admin
+@router.put("/cancel-by-user-id", response_model=list[Reservation] | None)
+def cancel_by_user_id(id: int, role: str = Depends(role_required(['Admin'])), db: Session = Depends(get_db)):
+    return try_perform(cancel_reservations_by_user_id, id, db=db)
